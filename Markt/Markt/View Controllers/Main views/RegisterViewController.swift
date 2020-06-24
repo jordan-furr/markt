fa5281466f135bef30376260ae01d24746c87a4a
Jordan Furr <you@example.com>
2020-06-23 20:27:45 -0400

    allow user to register, with error alerts, umich only, and scroll view

--- Markt/Markt/View Controllers/Main views/RegisterViewController.swift
+++ Markt/Markt/View Controllers/Main views/RegisterViewController.swift
@@ -9,14 +9,55 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
+    
+    @IBAction func registerTapped(_ sender: Any) {
+        guard let firstName = firstNameTextField.text, let lastName = lastNameTextField.text, let email = emailTextField.text,
+            let password = passwordTextField.text, let confirm = confirmTextField.text else {return}
+        
+        //MARK: MAKE SURE USER CAN REGISTER
+        if email.components(separatedBy: "@").last != "umich.edu" {
+            presentNonUmichEMailALert()
+        }
+        
+        if email == "" || firstName == "" || lastName == "" {
+            presentFillInTextFieldsAlert()
+            return
+        }
+        if password != confirm {
+            presentMisMatchPasswordAlert()
+            return
+        }
+        if password.count < 6 {
+            presentShortPasswordAlert()
+            return
+        }
+        
+        UserController.shared.signup(firstName: firstName, lastName: lastName, email: email, password: password) { (success, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                self.presentRegisterAlertView(error: error)
+            } else {
+                self.performSegue(withIdentifier: "newUser", sender: self)
+            }
+        }
+    }
+    
+    
+    //MARK: HELPER IB ACTIONS
+    
     @IBAction func backgroundTapped(_ sender: Any) {
         self.view.endEditing(true)
     }
@@ -25,14 +66,44 @@ class RegisterViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    //MARK: HELPER ALERTS
+    
+    func presentMisMatchPasswordAlert(){
+        let alertController = UIAlertController(title: "Passwords do not match", message: "Please re-type password", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alertController.addAction(defaultAction)
+        self.present(alertController, animated: true, completion: nil)
     }
-    */
-
+    
+    func presentShortPasswordAlert(){
+        let alertController = UIAlertController(title: "Password is too short", message: "Password must be at least 6 characters", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alertController.addAction(defaultAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func presentFillInTextFieldsAlert() {
+        let alertController = UIAlertController(title: "Missing Info", message: "Please fill in all fields", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alertController.addAction(defaultAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func presentNonUmichEMailALert() {
+        let alertController = UIAlertController(title: "Unable to register user", message: "A umich.edu email address is required to register for Markt", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alertController.addAction(defaultAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func presentRegisterAlertView(error: Error) {
+        let alertController = UIAlertController(title: "Error Creating Account", message: error.localizedDescription, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        present(alertController, animated: true, completion: nil)
+    }
 }
+
+
+
